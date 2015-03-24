22e4be982f8c441d069081a7a1e966007af78fdc
thijsh <git@th-d.nl>
2015-03-24 16:50:03 +0100

    wurk wurk

--- mongotime.sh
+++ mongotime.sh
@@ -6,14 +6,14 @@ do
   x=$((d-t))
   t=$(date +"%s%N")
   r=1
-  # Only when greater then 500 msec and not the first UNIX time round
-  if [ $x -gt 500000000 ] && [ $x -lt 1425432109876543210 ]
+  # Only when greater then 5000 msec and not the first UNIX time round
+  if [ $x -gt 5000000000 ] && [ $x -lt 1425432109876543210 ]
   then
     echo $x
   fi
   until [ $r -eq 0 ]; do
     # mongo 145.100.108.226/daffe --eval "db.dummy.count()" 2> /dev/null > /dev/null
-    mongo 145.100.108.227/daffe --eval "db.dummy.insert({y:1})" 2> /dev/null > /dev/null
+    mongo 54.155.150.78/daffe --eval "db.dummy.insert({y:1})" 2> /dev/null > /dev/null
     r=$?
   done
 done
